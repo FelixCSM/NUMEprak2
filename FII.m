104b5c62a2f37ab10d9a8a568ba0439682841aa2
FelixCSM <s9442559@NPLP29.pool.math.tu-dresden.de>
2016-01-08 17:51:46 +0100

    first try implementation

--- FII.m
+++ FII.m
@@ -1,7 +1,9 @@
-function [ output_args ] = FII( input_args )
+function [ F ] = FII( x1 , x2 )
 %FII Summary of this function goes here
 %   Detailed explanation goes here
 
+F(1)=sin(x1-2)*cos(x2);
+F(2)=x1^2-4*x1+x2^2+1;
 
 end
 
