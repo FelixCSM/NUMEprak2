05f3eb22d0437c865f4bbd3fd9da271a27b97036
FelixCSM <felix-voelker@web.de>
2016-01-08 18:04:09 +0100

    first try implementation

--- FIb.m
+++ FIb.m
@@ -1,7 +1,10 @@
-function [ output_args ] = FIb( input_args )
+function [ F ] = FIb( x1, x2, x3 )
 %FIB Summary of this function goes here
 %   Detailed explanation goes here
 
+F(1)= -x1 -x1^2 + 3*x1*x2 + 2*x2*x3;
+F(2)= 2*x1*x2 + 3*x2 + 3*x3 -3;
+F(3)= -3*x1*x2 - 5*x2 - 4*x3 + x3^2 +3;
 
 end
 
