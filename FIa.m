c71da118856b3ac5fe2723fa575a19ce58fd4751
FelixCSM <felix-voelker@web.de>
2016-01-08 18:03:40 +0100

    first try implementations

--- FIa.m
+++ FIa.m
@@ -1,7 +1,10 @@
-function [ output_args ] = FI( input_args )
+function [ F ] = FIa( x1 , x2 ,x3 )
 %FI Summary of this function goes here
 %   Detailed explanation goes here
 
+F(1)= -x1 -x1^2 + 3*x1*x2 + 2*x2*x3;
+F(2)= 2*x1*x2 + 3*x2 + 3*x3 -3;
+F(3)= -3*x1*x2 - 5*x2 - 4*x3 + x3^2 +3;
 
 end
 
