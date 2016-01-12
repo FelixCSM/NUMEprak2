abe3dcd20ac83716525e48ece9a49c8b8015faf3
FelixCSM <Felix.Voelker@tu-dresden.de>
2016-01-12 16:38:58 +0100

    implementet the jacobians

--- JFII.m
+++ JFII.m
@@ -1,6 +1,11 @@
-function [ output_args ] = JFII( input_args )
+function [ J ] = JFII( x1 , x2 )
 %JFII Summary of this function goes here
 %   Detailed explanation goes here
+
+J(1,1) = cos(x1-2)*cos(x2);
+J(1,2) = -sin(x1-2)*sin(x2);
+J(2,1) = 2*x1-4;
+J(2,2) = 2*x2;
 
 
 end
