9f6395bedc0b8af134062d370d7e24ff5955ea7a
FelixCSM <Felix.Voelker@tu-dresden.de>
2016-01-13 10:54:12 +0100

    wrote task

--- newt.m
+++ newt.m
@@ -1,4 +1,4 @@
-function [ F , X ] = newt( x, @f, @J, n, ged, gam, rho, tol, sol )
+function [ F , X ] = newt( x, f, J, n, ged, gam, rho, tol, check )
 %NEWT Summary of this function goes here
 %   Detailed explanation goes here
 
---
+++ task.m
@@ -0,0 +1,44 @@
+% I)
+
+% a)
+  
+  x0 = [ 0.1 , 0.1 , 0.9 ];
+  x = [ 0 , 0 , 1 ];
+  n = 20;
+  tol = 20^(-16);
+  newt( x0 , @FIa , @JFIa , n , false , 0 , 0 , tol , x)
+  
+  pause
+  
+% b)
+  
+  newt( x0 , @FIb , @JFIb , n , false , 0 , 0 , tol , x)
+  
+  pause
+  
+% II )
+  
+  x = [ 0 , 0 ];
+  tol = 10^(-10);
+  n = 50;
+  x0 = [ 3 ,1 ];
+  
+  newt( x0 , @FII , @JFII , n , false , 0 , 0 , tol , x)
+  
+  pause
+  
+  x0 = [ 5 ,3 ];
+  
+  newt( x0 , @FII , @JFII , n , false , 0 , 0 , tol , x)
+  
+  pause
+  
+  x0 = [ 4.7 , 2.7 ];
+  gam = [ 0.9 , 0.5 , 0.9 , 0.5];
+  rho = [ 0.5 , 0.5 , 0.1 , 0.1];
+ 
+  for i = 1:4
+      newt( x0 , @FII , @JFII , n , true , gam(i) , rho(i) , tol , x)  
+      pause
+  end    
+      
