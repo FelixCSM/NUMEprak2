52b2a418f19c53cda8c4e8a607a49417af6ca65d
FelixCSM <Felix.Voelker@tu-dresden.de>
2016-01-15 17:39:58 +0100

    implemented the normal newtwon algorithm, the if-else condition seemse to be ignored tho

--- newt.m
+++ newt.m
@@ -1,7 +1,22 @@
-function [ F , X ] = newt( x, f, J, n, ged, gam, rho, tol, check )
+function [ F , X ] = newt( x, f, J, n, ged, gamma, rho, tol, check )
 %NEWT Summary of this function goes here
 %   Detailed explanation goes here
 
-
+if not(ged)
+    
+    k=0;
+    xk = x;
+    while norm( f(xk)) > tol
+        xk= gaus( J(xk) , ( -f(xk)+ transpose(mtimes(J(xk),transpose(xk)))));
+        k=k+1;
+    end
+    X = xk;
+    F = f(X);
+    
+else
+    a = 0;
+    
+    
+end
 end
 
